8e423dfe567809cb66d4426853aa25d2dcce10c6
avdey <avdey87@gmail.com>
2017-10-24 17:54:30 +0300

    414 Создание явных интентов на стадии выполнения

--- app/src/main/java/com/aavdeev/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/aavdeev/nerdlauncher/NerdLauncherFragment.java
@@ -1,5 +1,6 @@
 package com.aavdeev.nerdlauncher;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -38,13 +40,14 @@ public class NerdLauncherFragment extends Fragment {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         //mRecyclerView устанавливаем в LayoutManager менеджер макета текущую активити
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
         //возвращаем вьюшку
         return view;
     }
 
 
     //адаптер
-    private void setAdapter() {
+    private void setupAdapter() {
 
         //создаем новый интент который просматривает манифесты с экшеном ACTION_MAIN
         Intent startupIntent = new Intent(Intent.ACTION_MAIN);
@@ -71,7 +74,55 @@ public class NerdLauncherFragment extends Fragment {
 //ввыводим лог файл в который пишем сколько найдена активностей(activities.size())
         //и имя активностей
         Log.i(TAG, "Found " + activities.size() + " activities.");
+        mRecyclerView.setAdapter(new ActitvityAdapter(activities));
     }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder {
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView;
+        }
+
+        public void bindActivity(ResolveInfo resolveInfo) {
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+        }
+    }
+
+    private class ActitvityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+        private final List<ResolveInfo> mActivities;
+
+        public ActitvityAdapter(List<ResolveInfo> activities) {
+            mActivities = activities;
+        }
+
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
+    }
+
+
 
 
 }
